4580cf85d71373b27242a8d602755d8a62f8df89
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-04-27 02:05:09 +0000

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,11 +8,11 @@ class StudentsController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("id")
+    the_id = params.fetch("path_id")
 
-    matching_students = Enrollment.where({ :id => the_id })
+    @matching_students = Enrollment.where({ :student_id => the_id }).at(0)
 
-    @the_student = matching_students.at(0)
+    @the_student = Student.where({:id => @matching_students.student_id}).at(0)
 
     render({ :template => "students/show.html.erb" })
   end
